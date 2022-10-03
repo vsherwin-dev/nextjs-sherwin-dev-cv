17b022e186d5af5b3419c20f60d069726f1c22fc
vsherwin-dev <100796048+vsherwin-dev@users.noreply.github.com>
2022-10-03 23:18:09 +0800

    list item mobile view

--- components/skills.js
+++ components/skills.js
@@ -40,7 +40,7 @@ export default function Skills(){
             <p className="text-[16px] font-mono uppercase hover:animate-pulse dark:text-white text-black"><span className="custom-underline">Knowledge</span></p>
           </div> 
           <div className="px-8 xl:px-64 font-mono dark:text-white text-black">
-            <ul className="list-disc grid overflow-hidden grid-cols-1 lg:grid-cols-2 pl-2 md:pl-14">
+            <ul className="list-disc grid grid-cols-1 lg:grid-cols-2 pl-2 md:pl-14">
               <li>Proficiency in Microsoft Office Tools.</li>
               <li>Knowledgeable in Application Testing. (UFT, Load Runner)</li>
               <li>Knowledgeable in MsSQL.</li>
@@ -54,7 +54,7 @@ export default function Skills(){
             <p className="text-[16px] font-mono uppercase hover:animate-pulse dark:text-white text-black"><span className="custom-underline">Certificates</span></p>
           </div> 
           <div className="px-8 xl:px-64 font-mono dark:text-white text-black">
-            <ul className="list-disc grid overflow-hidden grid-cols-1 pl-2 md:pl-14">
+            <ul className="list-disc grid grid-cols-1 pl-2 md:pl-14">
               <li>The 15th Youth Congress on Information Technology (Y4iT) November 7, 2017</li>
               <li>Fundamentals of Web Development February 9, 2019</li>
               <li>freeCodeCamp.org Responsive Web Design February 15, 2022</li>
